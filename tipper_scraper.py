ba8ac4892650ae7f8c615866def67e2f971d1f9f
Jacob Stollznow <jstollznow@hotmail.com>
2021-09-15 19:40:21 +1000

    make tipper scraper static for easy use across golf object stack

--- tipper_scraper.py
+++ tipper_scraper.py
@@ -1,8 +1,7 @@
 import re
 import requests
-import lxml
 from bs4 import BeautifulSoup
-from datetime import date, timedelta, datetime
+from datetime import datetime
 from bs4.element import SoupStrainer
 
 from tee_time_group import TeeTimeGroup
@@ -13,49 +12,50 @@ class TipperScraper:
     TIME_OBJECT = ''
     AVAILABLE_SLOT = ''
 
-    BOOKING_ENDPOINT = ''
+    TIMETABLE_ENDPOINT = ''
     ADD_TO_CART_ENDPOINT = ''
     CHECKOUT_ENDPOINT = ''
 
-    def __init__(self) -> None:
-        self.request_session = requests.session()
-        self.strainer_filter = SoupStrainer('div', TipperScraper.TEE_TIMES_LIST)
+    __request_session = None
+    __strainer_filter = None
 
     @staticmethod
-    def set_scraping_details(xml_config, endpoints_config):
+    def set_scraping_details(xml_config):
         TipperScraper.TEE_TIMES_LIST = re.compile(xml_config['TEE_TIMES_LIST'])
         TipperScraper.TEE_TIME_OBJECT = re.compile(xml_config['TEE_TIME_OBJECT'])
         TipperScraper.TIME_OBJECT = re.compile(xml_config['TIME_OBJECT'])
         TipperScraper.AVAILABLE_SLOT = re.compile(xml_config['AVAILABLE_SLOT'])
+        TipperScraper.__request_session = requests.session()
+        TipperScraper.__strainer_filter = SoupStrainer('div', TipperScraper.TEE_TIMES_LIST)
 
-        TipperScraper.BOOKING_ENDPOINT = endpoints_config['booking_timetable']
-        TipperScraper.ADD_TO_CART_ENDPOINT = endpoints_config['add_tee_time_to_cart']
-        TipperScraper.CHECKOUT_ENDPOINT = endpoints_config['checkout']
-
-    def get_tee_times_for_date(self, bookings_url, tee_time_cut_off, min_spots, booking_ids):
+    @staticmethod
+    def get_tee_times_for_date(bookings_url, tee_time_cut_off, min_spots, booking_ids):
         tee_times = set()
-        soup = BeautifulSoup(self.__get_content(bookings_url), 'lxml', parse_only=self.strainer_filter)
-        for row in soup.find_all('div', self.TEE_TIME_OBJECT):
-            tee_time = datetime.combine(tee_time_cut_off.date(), self.__get_tee_time(row).time())
+        soup = BeautifulSoup(TipperScraper.__get_content(bookings_url), 'lxml', parse_only=TipperScraper.__strainer_filter)
+        for row in soup.find_all('div', TipperScraper.TEE_TIME_OBJECT):
+            tee_time = datetime.combine(tee_time_cut_off.date(), TipperScraper.__get_tee_time(row).time())
             group_id = row.get('id')[4:]
-            booking_ids[group_id] = str(tee_time)
+            booking_ids[group_id] = tee_time
             if tee_time.time() <= tee_time_cut_off.time():
-                slots = self.__get_spots_available(row)
+                slots = TipperScraper.__get_spots_available(row)
                 if len(slots) >= min_spots:
                     tee_times.add(TeeTimeGroup(group_id, slots, tee_time))
             else:
                 break
         return tee_times
 
-    def __get_content(self, url):
-        return self.request_session.get(url).text
+    @staticmethod
+    def __get_content(url):
+        return TipperScraper.__request_session.get(url).text
 
-    def __get_tee_time(self, row):
-        time = datetime.strptime(row.find(self.TIME_OBJECT).text.strip(), '%I:%M %p')
+    @staticmethod
+    def __get_tee_time(row):
+        time = datetime.strptime(row.find(TipperScraper.TIME_OBJECT).text.strip(), '%I:%M %p')
         return time
 
-    def __get_spots_available(self, row):
+    @staticmethod
+    def __get_spots_available(row):
         slots = []
-        for slot in row.find_all("div", self.AVAILABLE_SLOT):
+        for slot in row.find_all("div", TipperScraper.AVAILABLE_SLOT):
             slots.append(slot.get('id'))
         return slots
